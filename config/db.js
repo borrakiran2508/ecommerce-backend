5d8050232376fddf04867a10f847e21dbadcb129
borrakiran2508 <borrakiran2508@gmail.com>
2026-06-16 01:51:26 +0530

    Changes db and index files

--- config/db.js
+++ config/db.js
@@ -5,9 +5,9 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected`);
   } catch (error) {
-    console.log(`MongoDB connection error`);
-   
-  }
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+}
 };
 
 module.exports = connectDB;
--- index.js
+++ index.js
@@ -29,5 +29,5 @@ app.use("/api/products", productRoutes);
 
 
 //server
-const PORT = 5000;
-app.listen(process.env.PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
